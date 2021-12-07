92e727cd16a4fd8f579c51f2ee8803cd744ba8c2
ShuaJJ <Sody008@gmail.com>
2021-12-07 10:07:36 +0800

    fixed some links

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -3,8 +3,8 @@ const serverBaseUrl = 'https://api.20y.org';
 
 export const githubUrl = githubBaseUrl;
 export const serverUrl = serverBaseUrl;
-export const twitterUrl = 'https://twitter.com/20Y_org';
-export const mediumUrl = 'https://blog.20y.org';
+export const twitterUrl = 'https://twitter.com/TheUnit_one';
+export const mediumUrl = 'https://blog.20y.org/theunit/home';
 export const discordUrl = 'https://discord.gg/4MjRckMJZN';
 export const valuesUrl = `${githubBaseUrl}/20y-values-paper/blob/main/toknowwhy-values.md`;
 export const unitPaperUrl = `${githubBaseUrl}/the-unit-paper/blob/main/the_unit_paper.pdf`;
